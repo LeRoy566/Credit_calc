d494f0ecf57ec02b2e6d5c7ae52c0a375918b51f
leoni <Lyunonin@yandex.ru>
2023-04-23 17:40:34 +0300

    Credit_calc_ 1Save

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Summ = document.getElementById('Summ');
 const Time = document.getElementById('Time');
 const Percent = document.getElementById('Percent');
 const button = document.querySelector('.button');
+const ctx = document.getElementById('myChart');
 button.addEventListener("click", function () {
     let a = Summ.value;
     let b = Time.value;
@@ -14,4 +15,37 @@ button.addEventListener("click", function () {
     let Perm5 = Perm1 - 1
     let Perm6 = Perm4 / Perm5
     console.log(Perm6);
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ["cd","dc"],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            borderWidth: 5
+          },
+          {
+            label: '# of Votes',
+            data: [2, 3],
+            borderWidth: 5
+          }]
+        },
+        options: {
+            plugins: {
+              title: {
+                display: true,
+                text: 'Chart.js Bar Chart - Stacked'
+              },
+            },
+            responsive: true,
+            scales: {
+              x: {
+                stacked: true,
+              },
+              y: {
+                stacked: true
+              }
+            }
+          }
+      });
 })
