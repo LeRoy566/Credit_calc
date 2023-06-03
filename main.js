d77ffaa921e5330348daf9af7afa3e6c1f6b076c
Leonid <lyunonin@yandex.ru>
2023-06-03 20:10:04 +0300

    Доделать время в таблице

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+const table = document.querySelector('table');
 const Summ = document.getElementById('Summ');
 const Time = document.getElementById('Time');
 const Percent = document.getElementById('Percent');
 const button = document.querySelector('.button');
-const button1 = document.querySelector('.button1')
+const button1 = document.querySelector('.button1');
+let now = new Date()
+alert(now)
 button.addEventListener("click", function () {
   let a = Summ.value;
   let b = Time.value;
   let c = Percent.value;
-  let d = 1 + 0.01 * (c / 12);``
+  let d = 1 + 0.01 * (c / 12); ``
   let Perm1 = d ** b
   let Perm2 = d - 1
   let Perm3 = Perm1 * Perm2
@@ -22,22 +25,37 @@ button1.addEventListener("click", function () {
   let c1 = Percent.value;
   let ostatok = [];
   for (let i = 1; i <= b1; i++) {
-    ostatok.push(((b1-(i-1))/b1)*a1)
-  } 
+    ostatok.push((((b1 - (i - 1)) / b1) * a1).toFixed(2))
+  }
   console.log(ostatok);
   let plate = [];
   for (let i = 0; i < b1; i++) {
-    if (i==0) {
-      plate.push(a1/b1+(c1/1200)*a1)
-    } else{
-      plate.push(a1/b1+c1/1200*(b1-i)/b1*a1)
+    if (i == 0) {
+      plate.push((a1 / b1 + (c1 / 1200) * a1).toFixed(2))
+    } else {
+      plate.push((a1 / b1 + c1 / 1200 * (b1 - i) / b1 * a1).toFixed(2))
     }
   }
   console.log(plate);
-  let credit =[];
-  for (let i = 1; i < +b1+1; i++) {
-    credit.push(c1/1200*((b1-(i-1))/b1*a1))
+  let credit = [];
+  for (let i = 1; i < +b1 + 1; i++) {
+    credit.push((c1 / 1200 * ((b1 - (i - 1)) / b1 * a1)).toFixed(2))
   }
   console.log(credit);
+  
+  for (let i = 0; i < b1; i++) {
+    table.style.opacity = 1
+    table.insertAdjacentHTML("beforeend",`<tr></tr>`)
+    const tr = document.querySelectorAll("tr");
+    const currentTr = tr[tr.length-1];
+      
+    currentTr.insertAdjacentHTML("beforeend",`<td>${i+1}</td>`);
+    currentTr.insertAdjacentHTML("beforeend",`<td>Месяц</td>`);
+    currentTr.insertAdjacentHTML("beforeend",`<td>${plate[i]}</td>`);
+    currentTr.insertAdjacentHTML("beforeend",`<td>${(a1/b1).toFixed(2)}</td>`);
+    currentTr.insertAdjacentHTML("beforeend",`<td>${credit[i]}</td>`);
+    currentTr.insertAdjacentHTML("beforeend",`<td>${ostatok[i]}</td>`);
+      
+  }
 }
 )
