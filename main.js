691177099d68c745ce2887d0a7d496bcacf2f0e2
Leonid <Lyunonin@yandex.ru>
2023-06-10 17:21:17 +0300

    разобраться с аннуитет формулами

--- main.js
+++ main.js
@@ -4,21 +4,17 @@ const Time = document.getElementById('Time');
 const Percent = document.getElementById('Percent');
 const button = document.querySelector('.button');
 const button1 = document.querySelector('.button1');
-let now = new Date()
-alert(now)
+const Months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
 button.addEventListener("click", function () {
   let a = Summ.value;
   let b = Time.value;
   let c = Percent.value;
-  let d = 1 + 0.01 * (c / 12); ``
-  let Perm1 = d ** b
-  let Perm2 = d - 1
-  let Perm3 = Perm1 * Perm2
-  let Perm4 = Perm3 * a
-  let Perm5 = Perm1 - 1
-  let Perm6 = Perm4 / Perm5
-  console.log(Perm6);
-});
+  let d = 1 + 0.01 * (c / 12);
+  let SummPlate = (((d**b)*(d-1))*a)/((d**b)-1)
+  console.log(SummPlate);
+}
+)
 button1.addEventListener("click", function () {
   let a1 = Summ.value;
   let b1 = Time.value;
@@ -42,20 +38,36 @@ button1.addEventListener("click", function () {
     credit.push((c1 / 1200 * ((b1 - (i - 1)) / b1 * a1)).toFixed(2))
   }
   console.log(credit);
-  
+
+  let date = [];
+  let newDate = new Date();
+  let month = newDate.getMonth();
+  let year = newDate.getFullYear();
+  for (let i = 0; i < b1; i++) {
+    date.push(Months[month] + " " + year)
+    month += 1
+    if (month >= 12) {
+      month = 0
+      year += 1
+    }
+  }
+
+  console.log(date);
+
   for (let i = 0; i < b1; i++) {
     table.style.opacity = 1
-    table.insertAdjacentHTML("beforeend",`<tr></tr>`)
+    table.insertAdjacentHTML("beforeend", `<tr></tr>`)
     const tr = document.querySelectorAll("tr");
-    const currentTr = tr[tr.length-1];
-      
-    currentTr.insertAdjacentHTML("beforeend",`<td>${i+1}</td>`);
-    currentTr.insertAdjacentHTML("beforeend",`<td>Месяц</td>`);
-    currentTr.insertAdjacentHTML("beforeend",`<td>${plate[i]}</td>`);
-    currentTr.insertAdjacentHTML("beforeend",`<td>${(a1/b1).toFixed(2)}</td>`);
-    currentTr.insertAdjacentHTML("beforeend",`<td>${credit[i]}</td>`);
-    currentTr.insertAdjacentHTML("beforeend",`<td>${ostatok[i]}</td>`);
-      
+    const currentTr = tr[tr.length - 1];
+
+    currentTr.insertAdjacentHTML("beforeend", `<td>${i + 1}</td>`);
+    currentTr.insertAdjacentHTML("beforeend", `<td>${date[i]}</td>`);
+    currentTr.insertAdjacentHTML("beforeend", `<td>${plate[i]}</td>`);
+    currentTr.insertAdjacentHTML("beforeend", `<td>${(a1 / b1).toFixed(2)}</td>`);
+    currentTr.insertAdjacentHTML("beforeend", `<td>${credit[i]}</td>`);
+    currentTr.insertAdjacentHTML("beforeend", `<td>${ostatok[i]}</td>`);
+
   }
+
 }
 )
